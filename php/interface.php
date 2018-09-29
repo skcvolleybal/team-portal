ed67595012f983b4ef7726800ceea3a665327d40
Jonathan Neuteboom <jonathan.neuteboom@gmail.com>
2018-09-30 00:02:27 +0200

    change

--- php/interface.php
+++ php/interface.php
@@ -3,13 +3,13 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\UseCases');
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\Common');
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\DomainEntities');
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\Gateways');
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\libs');
+set_include_path(get_include_path() . PATH_SEPARATOR . './UseCases');
+set_include_path(get_include_path() . PATH_SEPARATOR . './Common');
+set_include_path(get_include_path() . PATH_SEPARATOR . './DomainEntities');
+set_include_path(get_include_path() . PATH_SEPARATOR . './Gateways');
+set_include_path(get_include_path() . PATH_SEPARATOR . './libs');
 set_include_path(get_include_path() . PATH_SEPARATOR . '/public_html');
-set_include_path(get_include_path() . PATH_SEPARATOR . '.\Joomla');
+set_include_path(get_include_path() . PATH_SEPARATOR . './Joomla');
 
 require_once "TeamPortal.php";
 
