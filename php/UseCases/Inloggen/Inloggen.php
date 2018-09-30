1b8e2570e4028953635e69d22d12c874ec529abd
Jonathan Neuteboom <jonathan.neuteboom@gmail.com>
2018-09-30 21:00:32 +0200

    push

---
+++ php/UseCases/Inloggen/Inloggen.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'IInteractorWithData.php';
+include 'UserGateway.php';
+include 'NevoboGateway.php';
+
+class GetMijnOverzichtInteractor implements IInteractorWithData
+{
+   public function __construct($database)
+   {
+      $this->userGateway = new UserGateway($database);
+   }
+
+   private $userGateway;
+
+   public function Execute($data)
+   {
+      $username = $data->username;
+      $password = $data->password;
+      if ($this->userGateway->Login($username, $password)){
+         exit;   
+      }
+      else {
+         header("HTTP/1.1 500 Internal Server Error");
+         exit;
+      }
+   } 
+}
