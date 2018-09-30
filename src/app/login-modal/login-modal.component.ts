d7e65a89aefa298bdd01e3e40712a4d5171764c7
Jonathan Neuteboom <jonathan.neuteboom@gmail.com>
2018-09-30 15:41:47 +0200

    change

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
 
   ngOnInit() {
   }
