0fc6cd5850858e92cf706295f4316d70317e8d27
Jonathan Neuteboom <jonathan.neuteboom@gmail.com>
2018-09-30 16:06:35 +0200

    change

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-login-modal',
-  templateUrl: './login-modal.component.html',
-  styleUrls: ['./login-modal.component.scss']
+   selector: 'app-login-modal',
+   templateUrl: './login-modal.component.html',
+   styleUrls: ['./login-modal.component.scss']
 })
 export class LoginModalComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient) { }
+   constructor(private httpClient: HttpClient) { }
 
+   username: string;
+   password: string;
 
-  ngOnInit() {
-  }
+   login() {
+      this.httpClient.post<any>('https://www.skcvolleybal.nl/script/team-portal/php/interface/php?action=Login', {
+         username: this.username,
+         password: this.password
+      }).subscribe();
+   }
+
+   ngOnInit() {
+   }
 
 }
