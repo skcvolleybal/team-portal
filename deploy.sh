a52494553ce0453426f82e8d836d2008062aa886
J.I. Neuteboom <jonathan.neuteboom@gmail.com>
2019-09-06 14:53:18 +0200

    new deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-cd /home/deb105013n2/public_html/scripts/team-portal
-git pull
+ng build --prod
+rm  -R ./php/cache
+rm  -R ./php/errors
+ssh -T deb105013n2@skcvolleybal.nl -i /c/Users/jonat/.ssh/antagonist-ssh <<- 'END'
 cd /home/deb105013n2/public_html/team-portal
-rm * -R -v
-cp ../scripts/team-portal/dist/* . -R
-cd php
-mkdir cache
-curl https://www.skcvolleybal.nl/team-portal/php/interface.php?action=CompleteDailyTasks
+shopt -s extglob
+rm -R !("configuration.php")
+shopt -u extglob
+END
+scp -i /c/Users/jonat/.ssh/antagonist-ssh -r dist/* deb105013n2@skcvolleybal.nl:~/public_html/team-portal
+scp -i /c/Users/jonat/.ssh/antagonist-ssh -r php deb105013n2@skcvolleybal.nl:~/public_html/team-portal
