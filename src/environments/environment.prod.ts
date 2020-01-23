8a93424c1f5faa0e29925fdeeb0fc1b491ad82ca
J.I. Neuteboom <jonathan.neuteboom@gmail.com>
2020-01-23 23:56:19 +0100

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://www.skcvolleybal.nl/team-portal/php/interface.php'
+  baseUrl: 'https://www.skcvolleybal.nl/team-portal/api/'
 };
