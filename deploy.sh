33d141ab8173ef0e7c0d4c1b37ed51714a356552
J.I. Neuteboom <jonathan.neuteboom@gmail.com>
2020-01-29 20:05:18 +0100

    deplyment with tar

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,32 @@
+# build angular application
 npm run build
+
+# remove any caches
 rm  -R ./php/cache
 rm  -R ./php/errors
+
+# tar all files into 1 file
+cd dist && tar -cf ../deployment.tar * && cd -
+tar --append -f deployment.tar php static-pages
+
+# remove anything on server, except for Configuration.php
 ssh -T deb105013n2@skcvolleybal.nl -i /c/Users/jonat/.ssh/antagonist-ssh <<- 'END'
 cd /home/deb105013n2/public_html/team-portal
 shopt -s extglob
 rm -R !("Configuration.php")
 shopt -u extglob
 END
-scp -i /c/Users/jonat/.ssh/antagonist-ssh -r dist/* deb105013n2@skcvolleybal.nl:~/public_html/team-portal
-scp -i /c/Users/jonat/.ssh/antagonist-ssh -r php deb105013n2@skcvolleybal.nl:~/public_html/team-portal
-scp -i /c/Users/jonat/.ssh/antagonist-ssh -r static-pages deb105013n2@skcvolleybal.nl:~/public_html/team-portal
+
+# copy files to server
+scp -i /c/Users/jonat/.ssh/antagonist-ssh deployment.tar deb105013n2@skcvolleybal.nl:~/public_html/team-portal
+
+# Extract tar-file (+ remove afterwards) and move Configuration file to correct location
 ssh -T deb105013n2@skcvolleybal.nl -i /c/Users/jonat/.ssh/antagonist-ssh <<- 'END'
+cd /home/deb105013n2/public_html/team-portal
+tar -xf ./deployment.tar
 cp /home/deb105013n2/public_html/team-portal/Configuration.php /home/deb105013n2/public_html/team-portal/php/Configuration.php
+rm ./deployment.tar
 END
+
+# remove locally
+rm ./deployment.tar
