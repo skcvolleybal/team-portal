f840c4dd4eaf59f1dfbf51a53400a4f615b4af87
J.I. Neuteboom <jonathan.neuteboom@gmail.com>
2020-04-28 17:35:15 +0200

    protractor

---
+++ conf.js
@@ -0,0 +1,10 @@
+require('ts-node').register({
+  project: 'e2e/tsconfig.e2e.json' // if you have one
+});
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: [
+    //'src/app/barco/barcie-indeling/barcie-indeling.component.spec.ts',
+    './src/todo-spec.ts',
+  ],
+};
