1831074f19f420df412bfea45bf56089937e661b
Jonathan Neuteboom <jonathan.neuteboom@gmail.com>
2021-10-06 20:24:54 +0200

    removed android app

--- android-app/team-portal/build.sh
+++
@@ -1,4 +0,0 @@
-ionic cordova build android --prod --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore teamportal.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk TeamPortal
-rm TeamPortal.apk
-zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk TeamPortal.apk
