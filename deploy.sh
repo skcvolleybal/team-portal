d6a0c9c3a3dd7e6fd8579d810e15c05343af1711
KM Schellens <kmsch@users.noreply.github.com>
2023-01-21 19:45:10 +0100

    Update deploy.sh
    removed hardcoded sjon path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # build angular application
-npm i
+npm -f
 npm run build
 
 # build php code
@@ -17,7 +17,7 @@ cd dist && tar -cf ../deployment.tar * && cd -
 tar --append -f deployment.tar php static-pages
 
 # remove anything on server, except for Configuration.php
-ssh -T deb105013n2@skcvolleybal.nl -i /c/Users/jonat/.ssh/antagonist-ssh <<- 'END'
+ssh -T deb105013n2@skcvolleybal.nl -i ~/.ssh/antagonist-ssh <<- 'END'
 cd /home/deb105013n2/public_html/team-portal
 shopt -s extglob
 rm -R !("configuration.php"|".htaccess")
@@ -25,10 +25,10 @@ shopt -u extglob
 END
 
 # copy files to server
-scp -i /c/Users/jonat/.ssh/antagonist-ssh deployment.tar deb105013n2@skcvolleybal.nl:~/public_html/team-portal
+scp -i ~/.ssh/antagonist-ssh deployment.tar deb105013n2@skcvolleybal.nl:~/public_html/team-portal
 
 # Extract tar-file (+ remove afterwards) and move Configuration file to correct location
-ssh -T deb105013n2@skcvolleybal.nl -i /c/Users/jonat/.ssh/antagonist-ssh <<- 'END'
+ssh -T deb105013n2@skcvolleybal.nl -i ~/.ssh/antagonist-ssh <<- 'END'
 cd /home/deb105013n2/public_html/team-portal
 tar -xf ./deployment.tar
 cp /home/deb105013n2/public_html/team-portal/configuration.php /home/deb105013n2/public_html/team-portal/php/configuration.php
