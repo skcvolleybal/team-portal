86dcb2891266e5aa607da0a420b1fdca1800f8f3
KM Schellens <k.schellens@gmail.com>
2023-06-01 13:31:46 +0200

    Replaced configuration.php by dotenv

--- php/di-config.php
+++ php/di-config.php
@@ -1,7 +1,6 @@
 <?php
 // config.php
 
-use TeamPortal\Configuration;
 use TeamPortal\Gateways\JoomlaGateway;
 use TeamPortal\UseCases\IJoomlaGateway;
 use DI\Container;
@@ -12,10 +11,8 @@ use TeamPortal\UseCases\IBarcieGateway;
 use TeamPortal\UseCases\INevoboGateway;
 
 return [
-    IJoomlaGateway::class => DI\factory(function (Container $container) {
-        $configuration = $container->get(Configuration::class);
-        $database = $container->get(Database::class);
-        return new JoomlaGateway($configuration, $database);
+    IJoomlaGateway::class => DI\factory(function () {
+        return new JoomlaGateway();
     }),
     IBarcieGateway::class => DI\factory(function (Container $container) {
         $database = $container->get(Database::class);
