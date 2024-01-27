dc8be53046b7a93ff759320a31daf01463097edf
KM Schellens <k.schellens@gmail.com>
2024-01-27 15:49:56 +0100

    Init

--- src/app/emails/emails.component.ts
+++ src/app/emails/emails.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EmailsService } from '../core/services/emails.service';
 import { Router } from '@angular/router';
 import { faCheckCircle, faCogs, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { formatDate } from '@angular/common';
 
 
 @Component({
@@ -29,12 +30,23 @@ export class EmailsComponent {
 
   async ngOnInit() {
     this.getEmails();
+    
+  }
+
+  prepareEmails(emails) {
+    let previousDate = null;
+    for (let email of emails) {
+      const currentDate = formatDate(email.send_date, 'shortDate', 'en-US');
+      email.isNewDay = currentDate !== previousDate;
+      previousDate = currentDate;
+    }  
   }
   
   async getEmails() {
     this.emailsService.getEmails().subscribe(data => {
       this.emails = data; //  
       this.loading = false;
+      this.prepareEmails(this.emails);
       return data;
     }, error => {
       this.errorMessage = error.error.message
